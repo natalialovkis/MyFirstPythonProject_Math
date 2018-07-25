7cce4718f5ebd6e25ba331803ef39d3739ec9ee0
natalialovkis <41482665+natalialovkis@users.noreply.github.com>
2018-07-24 23:06:20 -0400

    improved file after PEP8 check!

--- math_project1.py
+++ math_project1.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
- 
-#Math learning program
+
+# Math learning program
 import random
 
 print("Hello Dear User!")
@@ -9,11 +9,12 @@ print()
 print("Hello %s! Let`s learn the Math!" % name)
 print()
 
-    
+
 num_of_ex = 0
 while True:
     try:
-        num_of_ex = int(input("How many exercises are you going to solve? Enter a number from 1 to 10: "))
+        num_of_ex = int(input("How many exercises are you going to solve? "
+                              "Enter a number from 1 to 10: "))
         if 1 <= num_of_ex <= 10:
             break
         print("try again")
@@ -22,53 +23,51 @@ while True:
 
 user_try = 0
 while user_try < num_of_ex:
-    first = random.randint(1,11) #generate number between 1 and 100
-    second = random.randint(1,11)
-    action = random.randint(1,4) # 1=+; 2=-; 3=*; 
-    
+    first = random.randint(1, 11)  # generate number between 1 and 100
+    second = random.randint(1, 11)
+    action = random.randint(1, 4)  # 1=+; 2=-; 3=*;
+
     if action == 1:
         answer = first + second
         try:
-            user_input = int(input("What is result of %d + %d :" %(first,second)))
+            user_input = int(input("What is result "
+                                   "of %d + %d :" % (first, second)))
             if user_input == answer:
                 print("Good!")
             else:
                 print("Not right")
-            
+
         except ValueError:
-            print("That's not an int!")    
-       
+            print("That's not an int!")
+
     elif action == 2:
         answer = first - second
         try:
-            user_input = int(input("What is result of %d - %d :" %(first,second)))
+            user_input = int(input("What is result "
+                                   "of %d - %d :" % (first, second)))
             if user_input == answer:
                 print("Good!")
             else:
                 print("Not right")
-            
+
         except ValueError:
-            print("That's not an int!")   
-            
+            print("That's not an int!")
+
     else:
         answer = first * second
         try:
-            user_input = int(input("What is result of %d * %d :" %(first,second)))
+            user_input = int(input("What is result "
+                                   "of %d * %d :" % (first, second)))
             if user_input == answer:
                 print("Good!")
             else:
                 print("Not right")
-            
-        except ValueError:
-            print("That's not an int!")    
 
-            
+        except ValueError:
+            print("That's not an int!")
     user_try += 1
-    
+
 print()
 print("Good job! Bye!")
 print()
 input("Press enter to exit.")
-
-        
-    
