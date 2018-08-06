e90a460a656c034dfc50e51f0272b9a2445793cd
natalialovkis <41482665+natalialovkis@users.noreply.github.com>
2018-08-06 17:00:55 -0400

    turtle changes

--- math_project1.py
+++ math_project1.py
@@ -90,27 +90,21 @@ print("Good job! Bye!")
 print()
 input("Press enter to see a surprise.")
 
-radius = 100
-petals = num_of_ex
+turtle.setup(800, 600)
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+wn.title("Surprize")
 
+tess = turtle.Turtle()
+tess.shape("turtle")
+tess.color("blue")
 
-def draw_arc(b, r):
-    c = 2*math.pi*r
-    ca = c/(360/60)
-    n = int(ca/3)+1
-    l = ca/n
-    for i in range(n):
-        b.fd(l)
-        b.lt(360/(n*6))
+tess.penup()
+size = 20
+for i in range(num_of_ex + 20):
+    tess.stamp()
+    size = size + 3
+    tess.forward(size)
+    tess.right(24)
 
-
-def draw_petal(b, r):
-    draw_arc(b, r)
-    b.lt(180-60)
-    draw_arc(b, r)
-    b.rt(360/petals-30)
-bob = turtle.Turtle()
-
-for i in range(petals):
-    draw_petal(bob, radius)
-    bob.lt(360/4)
+wn.exitonclick()
